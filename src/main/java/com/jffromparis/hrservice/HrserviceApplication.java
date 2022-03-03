01f2a5d24620c4efce4f946458a130a33788cd6a
Jean-françois LEPANTE <jean-francois@homypay.com>
2022-03-03 10:47:22 +0100

    project initialised

---
+++ src/main/java/com/jffromparis/hrservice/HrserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jffromparis.hrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jffromparis/hrservice/HrserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jffromparis.hrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
